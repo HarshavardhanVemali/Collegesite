49555483fffa119416ca4d994f063de3e99c095a
HarshavardhanVemali <vemalivardhan@gmail.com>
2024-08-01 19:56:46 +0530

    changescommit

--- collegesite/urls.py
+++ collegesite/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('testpage/',views.testpage,name='testpage'),
+    path('keytonespeakers/',views.keytonespeakers,name='keytonespeakers'),
+    path('registation/',views.registration,name='registration'),
+    path('venue/',views.venue,name='venue'),
 
 ]
--- collegesiteapp/views.py
+++ collegesiteapp/views.py
@@ -5,3 +5,12 @@ def index(request):
     return render(request,'index.html')
 def testpage(request):
     return render(request,'testpage.html')
+
+def keytonespeakers(request):
+    return render(request,'keytonespeakers.html')
+
+def registration(request):
+    return render(request,'registration.html')
+
+def venue(request):
+    return render(request,'venue.html')
